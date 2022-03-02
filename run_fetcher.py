b772901ed3baba96be09c909e341fc9968782f01
zhangziyuan <zhangziyuan@intraos.nsfocus.com>
2022-03-02 17:02:25 +0800

    优化

--- run_fetcher.py
+++ run_fetcher.py
@@ -10,12 +10,14 @@ from config import FETCHER_MAP
 def main():
     last_run_time = 0
     while True:
-        if time.time() - last_run_time > PROC_FETCHER_SLEEP:
+        check_time = time.time()
+        if check_time - last_run_time > PROC_FETCHER_SLEEP:
+            last_run_time = check_time
             for Fetcher in FETCHER_MAP.values():
                 fetcher = Fetcher()
                 fetcher.run()
         else:
-            time.sleep(PROC_FETCHER_SLEEP/2)
+            time.sleep(PROC_FETCHER_SLEEP/3)
 
 if __name__ == '__main__':
     main()
