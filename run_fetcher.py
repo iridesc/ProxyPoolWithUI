886f65a957825e267c14150de82bf604daf191e0
KleeX <>
2022-07-11 20:00:39 +0800

    修复验证器bug&代码优化

--- run_fetcher.py
+++ run_fetcher.py
@@ -8,9 +8,11 @@ import os
 import django
 import importlib
 
+from ProxyPool.settings import MAX_ALIVE_PROXY_AMOUNT
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProxyPool.settings")
 django.setup()
-from proxy_api.models import Fetcher, StatusRecode
+from proxy_api.models import Fetcher, StatusRecode, Proxy
 from fetchers.BaseFetcher import BaseFetcher
 
 VALID_FETCHERS = (
@@ -20,34 +22,36 @@ VALID_FETCHERS = (
 
 def main():
     while True:
+        proxies = Proxy.objects.filter(
+            to_validate_time__lt=time.time()).order_by("validated").order_by("to_validate_time")
+        if proxies.count() < MAX_ALIVE_PROXY_AMOUNT:
+            for fetcher_file in os.listdir("fetchers"):
+                # 过滤掉非目标的文件 和 基础类
+                if not fetcher_file.endswith("Fetcher.py") or fetcher_file == "BaseFetcher.py":
+                    continue
 
-        for fetcher_file in os.listdir("fetchers"):
-            # 过滤掉非目标的文件 和 基础类
-            if not fetcher_file.endswith("Fetcher.py") or fetcher_file == "BaseFetcher.py":
-                continue
+                # 新增fetcher过滤
+                if not fetcher_file.startswith(tuple([valid_prefix.rstrip("*") for valid_prefix in VALID_FETCHERS])):
+                    continue
 
-            # 新增fetcher过滤
-            if not fetcher_file.startswith(tuple([valid_prefix.rstrip("*") for valid_prefix in VALID_FETCHERS])):
-                continue
+                # 获取类名
+                fetcher_class_name = fetcher_file.split(".")[0]
+                # 根据类名获取FetcherClass
+                FetcherClass = getattr(importlib.import_module(f"fetchers.{fetcher_class_name}"), fetcher_class_name)
+                # 根据类名检查是否已经同步到数据库
+                fetcher_objs = Fetcher.objects.filter(name=fetcher_class_name)
+                if fetcher_objs.count() == 0:
+                    # 没有同步 则创建
+                    fetcher_obj = Fetcher()
+                    fetcher_obj.name = fetcher_class_name
+                    fetcher_obj.save()
+                else:
+                    # 已存在则取出
+                    fetcher_obj = fetcher_objs[0]
 
-            # 获取类名
-            fetcher_class_name = fetcher_file.split(".")[0]
-            # 根据类名获取FetcherClass
-            FetcherClass = getattr(importlib.import_module(f"fetchers.{fetcher_class_name}"), fetcher_class_name)
-            # 根据类名检查是否已经同步到数据库
-            fetcher_objs = Fetcher.objects.filter(name=fetcher_class_name)
-            if fetcher_objs.count() == 0:
-                # 没有同步 则创建
-                fetcher_obj = Fetcher()
-                fetcher_obj.name = fetcher_class_name
-                fetcher_obj.save()
-            else:
-                # 已存在则取出
-                fetcher_obj = fetcher_objs[0]
-
-            if time.time() - fetcher_obj.last_fetch_time > FetcherClass.fetch_gap and fetcher_obj.enable:
-                fetcher = FetcherClass(fetcher_obj)
-                fetcher.run()
+                if time.time() - fetcher_obj.last_fetch_time > FetcherClass.fetch_gap and fetcher_obj.enable:
+                    fetcher = FetcherClass(fetcher_obj)
+                    fetcher.run()
 
         time.sleep(BaseFetcher.fetch_gap / 3)
         # 记录系统状态
